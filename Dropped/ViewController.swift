94afaf9ee90968e97062225537daf091205df0b9
Loredana Crisan <loredanasf@gmail.com>
2014-09-15 19:02:38 -0700

    Attempted swipe, not sure it works

--- Dropped/ViewController.swift
+++ Dropped/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func goToNext(sender: UISwipeGestureRecognizer) {
+        
+        performSegueWithIdentifier("welcome2", sender: self)
+    }
 
 }
 
---
+++ Dropped/WelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeViewController.swift
+//  Dropped
+//
+//  Created by Loredana Crisan on 9/15/14.
+//  Copyright (c) 2014 Loredana Crisan. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
